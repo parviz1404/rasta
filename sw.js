2f64ea157cca958beb5367bd16ed8855306f74ae
parviz1404 <par.viz4@gmail.com>
2025-09-06 14:55:46 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,9 @@
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('rasta-v1').then(c => c.addAll([
-    './', './index.html', './manifest.webmanifest'
-  ])));
+  e.waitUntil(caches.open('rasta-v2').then(c => c.addAll(['./','./index.html','./manifest.webmanifest'])));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== 'rasta-v1').map(k => caches.delete(k)))
-  ));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k!=='rasta-v2').map(k => caches.delete(k)))));
 });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
